0a55c7484c29f866e9050a1466fa66a8fc4a876f
Zipofar Ago <admin@zipofar.ru>
2018-11-13 18:50:52 +0300

    Add views

---
+++ app/resources/views/error.php
@@ -0,0 +1,15 @@
+
+<?php require_once 'templates/header.php' ?>
+<?php require_once 'templates/form_test.php' ?>
+
+<div class="row">
+    <div class="col-md-12">
+        <h2>Error</h2>
+        <div class="alert alert-danger" role="alert">
+            <?= $error ?>
+        </div>
+    </div>
+</div>
+
+
+<?php require_once 'templates/footer.php' ?>
---
+++ app/resources/views/templates/form_test.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-md-12">
+        <h1>Page speed optimization</h1>
+        <form class="form-inline" action="/analyze" method="post">
+            <div class="form-group mx-sm-3 mb-2">
+                <label for="inputPassword2" class="sr-only">Password</label>
+                <input type="text" class="form-control" name="url" placeholder="Enter webpage url">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">TEST</button>
+        </form>
+    </div>
+</div>
