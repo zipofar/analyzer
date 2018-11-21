e7e281b4782b66caad77e61a049e014b347f40d3
Zipofar Ago <admin@zipofar.ru>
2018-11-21 18:35:43 +0300

    Remove unused functions

--- app/app/Misc/Parser.php
+++ app/app/Misc/Parser.php
@@ -50,42 +50,6 @@ class Parser
         return $arrTags;
     }
 */
-    public static function getTagLink($html)
-    {
-        $pattern = '/<link.*?>/mi';
-        preg_match_all($pattern, $html, $matches);
-        return $matches[0];
-    }
-
-    public static function isStyleSheetTag($tag)
-    {
-        return mb_stripos($tag, 'rel="stylesheet"') === false ? false : true;
-    }
-
-    public static function getHrefFromTag($tag)
-    {
-        $pattern = '/href="(?P<href>.*?)"/im';
-        preg_match($pattern, $tag, $matches);
-        return $matches['href'] ?? null;
-    }
-
-    public static function getSrcFromTag($tag)
-    {
-        $pattern = '/src="(?P<src>.*?)"/im';
-        preg_match($pattern, $tag, $matches);
-        return $matches['src'] ?? null;
-    }
-
-    public static function getTagName($tag)
-    {
-        $pattern = '/<(?P<tagname>\w*)(\s|>)/mi';
-        preg_match_all($pattern, $tag, $matches);
-        if (!isset($matches[0]['tagname'])) {
-            throw new \Exception('Can not resolve tag name');
-        }
-        return $matches[0]['tagname'];
-    }
-
     public static function getHttpMethod($url)
     {
         //$pattern = '/(?P<method>https{0,1}):\/\/(?P<domain>.*?)(\/|\?|$)/i';
